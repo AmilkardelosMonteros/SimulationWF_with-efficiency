0d111c6dd6f8184cab44585bca2bbbf5f403bf61
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-03-26 21:01:52 -0600

    Update WF_efi_dis.py
    I am adding the selection parameter

--- WF_efi_dis.py
+++ WF_efi_dis.py
@@ -16,6 +16,7 @@ vector_inicial = []
 
 kapa = 0.4
 N = 20**2#Cantidad de Recursos
+s = 0.008#parametro de la seleccion
 
 size0 = 10
 size1 = 5
@@ -41,6 +42,11 @@ def Y_n(vector):
 def suerte1(vector):
 	return Bernoulli(Y_n(vector))
 
+def suerte2(vector):
+	x = Y_n(vector)
+	proba = (x)/(x + (1-x)*(1-s)) #assuming that the efficient ones have an advantage s
+	return Bernoulli(proba)
+
 def Consumo(vector):
 	suma1 = 0
 	suma0 = 0
@@ -61,13 +67,26 @@ def Proceso1(n):
 		proceso.append(vec_aux)
 	return(proceso)
 
+
+def Proceso2(n):
+	proceso = [vector_inicial]
+	for i in range(n):
+		vec_aux = [suerte2(proceso[i])]
+		while Consumo(vec_aux) < N:
+			vec_aux.append(suerte2(proceso[i]))
+		proceso.append(vec_aux)
+	return(proceso)
+
 def Frecuencia(Proceso):
 	vector = []
 	for i in range(len(Proceso)):
 		vector.append(Y_n(Proceso[i]))
 	return vector
 
-X_n = Proceso1(300)
+#X_n = Proceso1(300)
+X_n = Proceso2(300)
 Y = Frecuencia(X_n)
 x = np.linspace(0,len(X_n),len(X_n))
 grafica(x,Y)
+
+
