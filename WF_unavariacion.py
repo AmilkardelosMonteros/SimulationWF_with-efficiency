1eaa72815b25fefe8593c5d0be09252d1bd37cbd
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-04-16 19:47:29 -0500

    Update WF_unavariacion.py
    I'm adding a lot of changes

--- WF_unavariacion.py
+++ WF_unavariacion.py
@@ -4,65 +4,133 @@ import numpy as np
 import random 
 import math
 import matplotlib.pyplot as plt
-#parametros = [0.2,0.3,0,0]#k1,k2,s1,s2
 x1 = 0.5
 N = 10**2
-	
+
+def grafica(x,y):
+	plt.plot(x,y,color='blue')
+	plt.xlabel('t')
+	plt.ylabel('Xt')
+	plt.title('Wright Fisher con Eficiencia k')
+	plt.show()
+        
 def graficaC(x,y):
-    plt.plot(x,y,color='blue')
+    plt.plot(x,y,color = 'navy')
     plt.xlabel('t')
     plt.ylabel('Wt')
+    plt.xlim(-1, 1)
+    plt.ylim(-1,1)
     plt.title('Caminata aleatoria en KxS')
     plt.show()
-
-def consumo(k1,k2,vector):
-        suma = 0
-        for i in range(len(vector)):
-                if vector[i]==1:
-                        suma = suma + (1-k1)
-                else:
-                        suma = suma + (1-k2)
-        return suma
-
-def frecuencia(vector):
-        return sum(vector)/len(vector)
-
-def ganador(k1,s1,k2,s2):
-        frec = x1
-        while frec!=0 and frec !=1:
-                vector_aux = []
-                while consumo(k1,k2,vector_aux) < N:
-                        x = frec
-                        p = ((1+s1)*x)/((1+s1)*x+(1+s2)*(1-x))
-                        vector_aux.append(np.random.binomial(1,p,1))
-                frec = frecuencia(vector_aux)
-        return frec
-
-def distribucion2():
-        v = True
-        while v:
-                x = 2*random.random()-1
-                y = 2*random.random()-1
-                if x**2 + y**2 <= 1:
-                        v = False
-        return x,y
-
-def distribucion3(eps1,eps2):
-        return eps1*np.random.uniform(-1,1,1),eps2*np.random.uniform(-1,1,1)
+    
+def ganador(k1,s1,k0,s0):
+    x = [x1]
+    while x[-1] != 0 and x[-1] != 1:
+        C = 0
+        D = 0 
+        M = 0
+        while C <= N:
+            f = x[-1]
+            A = np.random.binomial(1,((1+s1)*f)/((1+s1)*f+(1+s0)*(1-f)))
+            D += A
+            C += A*(1-k1) + (1-A)*(1-k0)
+            M += 1
+        x[0]=float(D)/M
+    return float(x[-1])
 
 
-def Caminata(n):
-        trayectoria = [[0,0]]
+
+def puntos(n,mu1,mu2):
+    perdedores = []
+    ganadores = []
+    for i in range(n):
+        muestra = [float(mu1*np.random.uniform(0,1,1)),float(mu2*np.random.uniform(-1,1,1))]#eficiencia,seleccion
+        if ganador(0,0,muestra[0],muestra[1]) == 0:
+            ganadores.append(muestra)
+        else:
+            perdedores.append(muestra)
+    return [perdedores,ganadores]
+    
+def grafica_puntos(puntos):
+    per = puntos[0]
+    gan = puntos[1]
+    x1 = []
+    y1 = []
+    x2 = []
+    y2 = []
+    for i in range(len(per)):
+        x1.append(per[i][0])
+        y1.append(per[i][1])
+    for j in range(len(gan)):
+        x2.append(gan[j][0])
+        y2.append(gan[j][1])
+    plt.plot(y1,x1,'ro')
+    plt.plot(y2,x2,'bo')
+    plt.show()
+#p = puntos(100,0.4,0.05)
+#grafica_puntos(p)
+
+
+
+def distribucion(v,mu1,mu2):
+        return v[-1][0]+ mu1*np.random.uniform(-1,1,1), v[-1][1] + mu2*np.random.uniform(-1,1,1)
+    
+def Mutacion1(n,mu1,mu2):
+    trayectoria =  [[0,0]]
+    for i in range(n):
+        print(trayectoria[-1])
+        retador = distribucion(trayectoria,mu1,mu2) #[float(mu1*np.random.uniform(0,1,1)),float(mu2*np.random.uniform(-1,1,1))]
+        if abs(trayectoria[-1][0]) >= 1 or abs(trayectoria[-1][1]) >= 1:  
+            return trayectoria
+        elif abs(retador[0]) >=1 or abs(retador[1]) >= 1:
+                return trayectoria
+        elif ganador(trayectoria[-1][0],trayectoria[-1][1],retador[0],retador[1]) == 1:#k1,s1,k2,s2
+            trayectoria.append([trayectoria[-1][0],trayectoria[-1][1]])
+        else:
+            trayectoria.append([retador[0],retador[1]])
+    return trayectoria
+                    
+            
+def Mutacion2(n,mu1,mu2):
+        trayectoria =  [[0,0]]
         for i in range(n):
-                retador = distribucion2()
-                if ganador(trayectoria[-1][0],trayectoria[-1][1],retador[0],retador[1]) == 0:#k1,s1,k2,s2
-                        trayectoria.append([retador[0],retador[1]])
-                else:
-                        trayectoria.append([trayectoria[-1][0],trayectoria[-1][1]])
-                        
-        return trayectoria 
+                print(trayectoria[-1])
+                volado = np.random.binomial(1,0.5,1)
+                if volado == 1: #Se muta solo el parametro de la eficiencia
+                        retador = distribucion(trayectoria,mu1,0)
+                        if abs(trayectoria[-1][0]) >= 1 or abs(trayectoria[-1][1]) >= 1:  
+                                return trayectoria
+                        elif abs(retador[0]) >=1 or abs(retador[1]) >= 1:
+                                return trayectoria
+                        elif ganador(trayectoria[-1][0],trayectoria[-1][1],retador[0],retador[1]) == 1:#k1,s1,k2,s2
+                                trayectoria.append([trayectoria[-1][0],trayectoria[-1][1]])
+                        else:
+                                trayectoria.append([retador[0],retador[1]])
+                                
 
-def dibuja_caminata(trayectoria):
+                else:
+                        retador = distribucion(trayectoria,0,mu2)
+                        if abs(trayectoria[-1][0]) >= 1 or abs(trayectoria[-1][1]) >= 1:  
+                                return trayectoria
+                        elif abs(retador[0]) >=1 or abs(retador[1]) >= 1:
+                                return trayectoria
+                        elif ganador(trayectoria[-1][0],trayectoria[-1][1],retador[0],retador[1]) == 1:#k1,s1,k2,s2
+                                trayectoria.append([trayectoria[-1][0],trayectoria[-1][1]])
+                        else:
+                                trayectoria.append([retador[0],retador[1]])
+
+        return trayectoria
+                        
+                                
+                        
+
+
+
+
+
+        
+
+def dibuja_mutacion1(trayectoria):
         x = []
         y = []
         for i in range(len(trayectoria)):
@@ -70,10 +138,8 @@ def dibuja_caminata(trayectoria):
                 x.append(v[0])
                 y.append(v[1])
         graficaC(x,y)
-	
-C=Caminata(10)        
-#print(C)
-dibuja_caminata(C)
 
-
-
+    
+C = Mutacion2(10,1,0.05)
+#print(len(C))
+dibuja_mutacion1(C)
