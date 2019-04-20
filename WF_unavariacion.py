5fe07f5e395320871c81c78dc2682576a8d72db5
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-04-19 20:18:53 -0500

    Update WF_unavariacion.py

--- WF_unavariacion.py
+++ WF_unavariacion.py
@@ -1,3 +1,4 @@
+
 #Gazque Espinosa de lo Monteros Edgar Amilkar
 from matplotlib import pyplot as plt
 import numpy as np
@@ -14,13 +15,13 @@ def grafica(x,y):
 	plt.title('Wright Fisher con Eficiencia k')
 	plt.show()
         
-def graficaC(x,y):
+def graficaC(x,y,pasos,rep):
     plt.plot(x,y,color = 'navy')
     plt.xlabel('t')
     plt.ylabel('Wt')
     plt.xlim(-1, 1)
     plt.ylim(-1,1)
-    plt.title('Caminata aleatoria en KxS')
+    plt.title('Caminata aleatoria_de'+str(pasos)+ '_en KxS con_'+ str(rep)+'_repeticiones')
     plt.show()
     
 def ganador(k1,s1,k0,s0):
@@ -39,12 +40,11 @@ def ganador(k1,s1,k0,s0):
     return float(x[-1])
 
 
-
 def puntos(n,mu1,mu2):
     perdedores = []
     ganadores = []
     for i in range(n):
-        muestra = [float(mu1*np.random.uniform(0,1,1)),float(mu2*np.random.uniform(-1,1,1))]#eficiencia,seleccion
+        muestra = [float(mu1*np.random.uniform(-1,1,1)),float(mu2*np.random.uniform(-1,1,1))]#eficiencia,seleccion
         if ganador(0,0,muestra[0],muestra[1]) == 0:
             ganadores.append(muestra)
         else:
@@ -58,16 +58,20 @@ def grafica_puntos(puntos):
     y1 = []
     x2 = []
     y2 = []
-    for i in range(len(per)):
+    p = len(per)
+    g = len(gan)
+    for i in range(p):
         x1.append(per[i][0])
         y1.append(per[i][1])
-    for j in range(len(gan)):
+    for j in range(g):
         x2.append(gan[j][0])
-        y2.append(gan[j][1])
-    plt.plot(y1,x1,'ro')
-    plt.plot(y2,x2,'bo')
+        y2.append(gan[j][1]) 
+    total = p + g
+    plt.plot(x1,y1,'ro')
+    plt.plot(x2,y2,'bo')
+    plt.title('Ganadores(azules)=' + str(g)+',Numero total de puntos =_' + str(total))
     plt.show()
-#p = puntos(100,0.4,0.05)
+#p = puntos(100,0.4,0.01)
 #grafica_puntos(p)
 
 
@@ -78,7 +82,7 @@ def distribucion(v,mu1,mu2):
 def Mutacion1(n,mu1,mu2):
     trayectoria =  [[0,0]]
     for i in range(n):
-        print(trayectoria[-1])
+        print(i)
         retador = distribucion(trayectoria,mu1,mu2) #[float(mu1*np.random.uniform(0,1,1)),float(mu2*np.random.uniform(-1,1,1))]
         if abs(trayectoria[-1][0]) >= 1 or abs(trayectoria[-1][1]) >= 1:  
             return trayectoria
@@ -94,13 +98,13 @@ def Mutacion1(n,mu1,mu2):
 def Mutacion2(n,mu1,mu2):
         trayectoria =  [[0,0]]
         for i in range(n):
-                print(trayectoria[-1])
                 volado = np.random.binomial(1,0.5,1)
                 if volado == 1: #Se muta solo el parametro de la eficiencia
                         retador = distribucion(trayectoria,mu1,0)
                         if abs(trayectoria[-1][0]) >= 1 or abs(trayectoria[-1][1]) >= 1:  
                                 return trayectoria
                         elif abs(retador[0]) >=1 or abs(retador[1]) >= 1:
+                                trayectoria.append([retador[0],retador[1]])
                                 return trayectoria
                         elif ganador(trayectoria[-1][0],trayectoria[-1][1],retador[0],retador[1]) == 1:#k1,s1,k2,s2
                                 trayectoria.append([trayectoria[-1][0],trayectoria[-1][1]])
@@ -122,24 +126,19 @@ def Mutacion2(n,mu1,mu2):
         return trayectoria
                         
                                 
-                        
-
-
-
-
-
-        
-
 def dibuja_mutacion1(trayectoria):
         x = []
         y = []
-        for i in range(len(trayectoria)):
+        repeticiones = 0
+        pasos = len(trayectoria)
+        for i in range(pasos):
+                if i > 0:
+                        if trayectoria[i][0] == trayectoria[i-1][0] and trayectoria[i][1] == trayectoria[i-1][1]:
+                                repeticiones += 1
                 v = trayectoria[i]
                 x.append(v[0])
                 y.append(v[1])
-        graficaC(x,y)
-
-    
-C = Mutacion2(10,1,0.05)
-#print(len(C))
-dibuja_mutacion1(C)
+        graficaC(x,y,pasos,repeticiones)
+        
+#C = Mutacion1(100,0.01,0.03)
+#dibuja_mutacion1(C)
