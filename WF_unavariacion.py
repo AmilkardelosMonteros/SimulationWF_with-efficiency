351a2b63753cbc71203ee388e98ea8a33708abe9
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-04-29 21:14:35 -0500

    Update WF_unavariacion.py

--- WF_unavariacion.py
+++ WF_unavariacion.py
@@ -142,3 +142,30 @@ def dibuja_mutacion1(trayectoria):
         
 #C = Mutacion1(100,0.01,0.03)
 #dibuja_mutacion1(C)
+def MB(n,T):
+	Z = []
+	particion = np.linspace(0,T,n)
+	for i in range(n):
+		Z.append((float(np.random.normal(0,1,1)))*(math.sqrt(T/n)))
+	S = np.cumsum(Z)
+	return particion,S
+    
+def WF_efic(alfa,kapa,frecuencia,T):
+    M = MB(4000,T)
+    X = [frecuencia]
+    t = list(M[0])
+    W = list(M[1])
+    i = 0
+    paro = True
+    while paro and i < len(t)-1:
+        if X[-1] >= 1 or X[-1]<= 0:
+            paro = False
+            return X
+        w = X[i]-alfa*X[i]*(t[i+1]-t[i]) + (math.sqrt(X[i]*(1-X[i])*(1-kapa*X[i])))*(W[i+1]-W[i])
+        i = i + 1
+        X.append(w)
+       
+    return X
+
+Proceso = WF_efic(0,0.25,0.5,2)
+print(Proceso)
