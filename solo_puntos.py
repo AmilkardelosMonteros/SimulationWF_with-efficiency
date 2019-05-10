afcfc54f2c1cc764510239ccf518f8806d28bd0c
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-05-09 21:21:27 -0500

    Update solo_puntos.py

--- solo_puntos.py
+++ solo_puntos.py
@@ -1,36 +1,40 @@
 import numpy as np
+import numpy as np
+
 x1 = 0.5
+
 N = 100
+
 def ganador(k1,s1,k0,s0):
-    x = [x1]
-    while x[-1] > 0 and x[-1] < 1:
+    x = x1
+    while x > 0 and x < 1:
         C = 0
-        D = 0 
+        D = 0
         M = 0
         while C < N:
-            f = x[-1]
-            print(f)
+            f = x
             A = np.random.binomial(1,((1+s1)*f)/((1+s1)*f+(1+s0)*(1-f)))
             D += A
             C += A*(1-k1) + (1-A)*(1-k0)
             M += 1
-        x[0]=float(D)/M
-    return int(float(x[-1]))
+        x=float(D)/M
+    return x
 
-ganador(1,0.1,0,0)
-def puntos(n,m,repeticiones,mu1,mu2):
-    S = np.linspace(-0.5,0.5,n)
+
+
+def puntos(n,m,repeticiones):
+    S = np.linspace(-0.05,0.05,n)
     K = np.linspace(0.3,0.7,m)
     Informacion = []
-    for s in range(len(S)):
-        for k in range(len(K)):
+    for s in S:
+        for k in K:
             porcentaje_ganador = 0
-            for i in range(repeticiones):
-                print(i)
+            for i in range(int(repeticiones)):
                 g = ganador(k,s,0,0)
+                print(g)
                 if g == 1:
                     porcentaje_ganador += 1/repeticiones
             Informacion.append([s,k,porcentaje_ganador])
     return Informacion
 
-#print(puntos(3,3,5,0.01,0.1))
+P = puntos(3,3,5.0)
