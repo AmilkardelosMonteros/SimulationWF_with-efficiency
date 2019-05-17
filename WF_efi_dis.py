fcf2a69bce83530c8d9a1d40ee3cf4fec71fa85f
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-05-17 14:39:42 -0500

    Update WF_efi_dis.py

--- WF_efi_dis.py
+++ WF_efi_dis.py
@@ -1,123 +1,69 @@
-#Gazque Espinosa de lo Monteros Edgar Amilkar
 from matplotlib import pyplot as plt
 import numpy as np
 import random 
 import math
-import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+        
 def grafica(x,y):
 	plt.plot(x,y,color='blue')
 	plt.xlabel('t')
 	plt.ylabel('Xt')
-	plt.title('Wright Fisher con Eficiencia k')
+	plt.title('Wright Fisher with-efficiency k')
 	plt.show()
-#0 son los eficientes 
-#1 los ineficientes
-vector_inicial = []
 
-kapa = 0.1
-N = 30**2#Cantidad de Recursos
-s = 0.08#parametro de la seleccion
-
-size0 = 10
-size1 = 5
-for i in range(size0):
-	vector_inicial.append(0)
-for i in range(size1):
-	vector_inicial.append(1)
-
-def Bernoulli(p):
-	b = random.random()
-	if b <= p:
-		return 1
-	else:
-                return 0
-
-def Y_n(vector):
-	suma = 0
-	for i in range(len(vector)):
-		if vector[i] == 1:
-			suma = suma + 1
-	return (suma)/len(vector)
-
-def suerte1(vector):
-	return Bernoulli(Y_n(vector))
-
-def suerte2(vector):
-	x = Y_n(vector)
-	proba = (x)/(x + (1-x)*(1-s)) #assuming that the efficient ones have an advantage s
-	return Bernoulli(proba)
-def suerte3(vector):
-	x = Y_n(vector)
-	proba = (x)/(x*(1-s)+(1-x)) #assuming that the efficient ones do not have an advantage s
-	return Bernoulli(proba)
-	
-def Consumo(vector):
-	suma1 = 0
-	suma0 = 0
-	for i in range(len(vector)):
-		if vector[i] == 0:
-			suma0 = suma0 + 1
-		else:
-			suma1 = suma1 + 1
-
-	return suma1 + (1-kapa)*suma0
-
-x = 0.5 #frecuencia de los eficientes
-def PROCESO(n):
-        proceso = [x]
-        for i in range(n):
-                vector_aux = []
-                while Consumo(vector_aux) < N:
-                        vector_aux.append(Bernoulli(proceso[i]))
-                proceso.append(Y_n(vector_aux))
-                if proceso[i] == 1 or proceso[i]==0:
-                        break
-        return proceso
-                        
-        
-def Proceso1(n):
-	proceso = [vector_inicial]
+def MB(n,T):
+	Z = []
+	particion = np.linspace(0,T,n)
 	for i in range(n):
-		vec_aux = [suerte1(proceso[i])]
-		while Consumo(vec_aux) < N:
-			vec_aux.append(suerte1(proceso[i]))
-		proceso.append(vec_aux)
-	return(proceso)
+		Z.append((float(np.random.normal(0,1,1)))*(math.sqrt(T/n)))
+	S = np.cumsum(Z)
+	return particion,S
+    
+def WF_efic(alfa,kapa,frecuencia,T):
+	M = MB(4000,T)
+	X = [frecuencia]
+	t = list(M[0])
+	W = list(M[1])
+	for i in range(len(t)-1):
+           
+            if X[i] >= 1:
+                w = 1
+                X.append(w)
+            elif X[i]<=0:
+                w = 0
+                X.append(w)
 
-def Proceso2(n):
-	proceso = [vector_inicial]
-	for i in range(n):
-		vec_aux = [suerte2(proceso[i])]
-		while Consumo(vec_aux) < N:
-			vec_aux.append(suerte2(proceso[i]))
-		proceso.append(vec_aux)
-	return(proceso)
+            else:
+                w = X[i]-alfa*X[i]*(1-X[i])*(t[i+1]-t[i]) + (math.sqrt(X[i]*(1-X[i])*(1-kapa*X[i])))*(W[i+1]-W[i])
+                X.append(w)
+	return t,X
 
-def Proceso3(n):
-	proceso = [vector_inicial]
-	for i in range(n):
-		vec_aux = [suerte3(proceso[i])]
-		while Consumo(vec_aux) < N:
-			vec_aux.append(suerte3(proceso[i]))
-		proceso.append(vec_aux)
-	return(proceso)
+Proceso = WF_efic(0,0.25,0.5,2)
+x = Proceso[0]
+y = Proceso[1]
+#grafica(x,y)
+fig, ax = plt.subplots()
+xdata, ydata = [], []
+ln, = plt.plot([], [], 'b-')
 
-def Frecuencia(Proceso):
-	vector = []
-	for i in range(len(Proceso)):
-		vector.append(Y_n(Proceso[i]))
-	return vector
+def init():
+    ax.set_xlim(0, 2)
+    ax.set_ylim(0, 1)
+    return ln,
 
-#X_n = Proceso2(200)
-#Y = Frecuencia(X_n)
-#x = np.linspace(0,len(X_n),len(X_n))
-#grafica(x,Y)
+def update(frame):
+    ln.set_data(x,y)
+    return ln,
 
-#X_n = Proceso3(100)
-#Y = Frecuencia(X_n)
-#x = np.linspace(0,len(X_n),len(X_n))
-#grafica(x,Y)
 
-y = PROCESO(100)
-x= np.linspace(0,len(y),len(y))
-grafica(x,y)
+def update(frame):
+    xdata.append(x[frame])
+    ydata.append(y[frame])
+    ln.set_data(xdata, ydata)
+    return ln,
+ani = FuncAnimation(fig, update, frames=range(len(x)),init_func=init, blit=True,interval = 20,repeat = True ,repeat_delay = 2)
+
+plt.xlabel('t')
+plt.ylabel('Xt')
+plt.title('Wright Fisher with-efficiency k')
+plt.show()
