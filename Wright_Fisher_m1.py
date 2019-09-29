f6d8dc082770be26fb8821e55524b246ea19e5ea
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-09-29 18:05:28 -0500

    Update Wright_Fisher_m1.py

--- Wright_Fisher_m1.py
+++ Wright_Fisher_m1.py
@@ -6,22 +6,25 @@ import time
 param = sys.argv[1]
 Param = int(param)
 x1 = 0.5
-N = 100
+N = 1000
 
-def ganador(k1,s1,k0,s0):
+def ganador(parametros):
+    k1 = parametros[0]
+    s1 = parametros[1]
+    k0 = parametros[2]
+    s0 = parametros[3]
     x = x1
     while x > 0 and x < 1:
         C = 0
-        D = 0
-        M = 0
-        while C < N:
+	D = 0
+	M = 0
+	while C < N:
             f = x
             A = np.random.RandomState().binomial(1,((1+s1)*f)/((1+s1)*f+(1+s0)*(1-f)))
             D += A
             C += A*(1-k1) + (1-A)*(1-k0)
             M += 1
         x=float(D)/M
-        print(x)
     return x
 
 def proba(k,s,repeticiones):
@@ -35,7 +38,7 @@ def proba(k,s,repeticiones):
     return todo
 
 def puntos(n,m,repeticiones,numero,nombre):
-    S = np.linspace(-0.05,0.05,n)
+    S = np.linspace(-0.025,0.025,n)
     s = S[numero]
     K = np.linspace(0,0.9,m)
     archivo = open(nombre,'a')
@@ -45,8 +48,9 @@ def puntos(n,m,repeticiones,numero,nombre):
     archivo.close()
     
 def funcion(numero):
-    nombre = 'm2_' + str(int(numero)) + '.txt'
-    puntos(20,20,5000,numero,nombre)
-    
-funcion(Param)
+    nombre = 'un_archivo' + str(int(numero)) + '.txt'
+    #archivo = open(nombre,'w')
+    #archivo.close()
+    puntos(20,20,1000,numero,nombre)
 
+funcion(Param)
