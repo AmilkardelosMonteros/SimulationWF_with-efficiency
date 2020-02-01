e683c3b15c6b5f088556be85507d5688470b1262
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2020-01-31 21:13:24 -0600

    Update Caminatas_exp2.py
    Habia un error, si las parametros transformados salen del dominio, hay que parar antes de calcular las probas, antes no era necesario

--- Caminatas_exp2.py
+++ Caminatas_exp2.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 u1 = 0.01 #Incremento en K (y)
 
-u2 = 0.001 #Incremento en S (x)
-frec_inicial = 0.01
+u2 = 0.1 #Incremento en S (x)
+frec_inicial = 0.5
 #Model M1
 def probafixWF(a,y): #para k = 0, (es un WF normal) a es la seleccion y y la frecuencia inicial
     p = (1-np.exp(- 2*a*y))/(1 - np.exp(-2*a))
@@ -29,9 +29,10 @@ def probabilidades(s,k):
     s_barra_neg  = -u2/(1+s)
     k_gorro = u1/(1-k)
     flag = False
-    if k_gorro > 1:
-        flag = True
-        #import pdb; pdb.set_trace()
+    if abs(s_barra) > 5:
+        print('S_gorro salio!!')
+        return [0.25,0.25,0.25,0.25],True
+    
     p_mas_mas  = probafixWF(s_barra,frec_inicial)
     p_menos_mas = p_mas_mas 
     p_menos_menos = probafixWF(s_barra_neg,frec_inicial)
@@ -43,12 +44,12 @@ def probabilidades(s,k):
     p_menos_menos =  p_menos_menos/Sum
     return [p_mas_mas,p_mas_menos,p_menos_mas,p_menos_menos],flag 
 
-archivo = open('Experimento2.txt','a')
+archivo = open('Exp2_s_5_frec_0.5.txt','w')
 
 def caminata():
     x = 0
     y = 0.5
-    while abs(x) < 0.05 and y < 1 and y > 0:
+    while abs(x) < 5 and y < 1 and y > 0:
         P,bandera =  probabilidades(x,y)
         if bandera == True:
             break
@@ -67,10 +68,35 @@ def caminata():
             x = x - u2
     np.savetxt(archivo,np.matrix([x,y]))
 
-
-for i in range(5000):
-    if i%10 == 0:
+for i in range(500):
+    if i%10:
         print(i)
-    caminata()       
-        
+    caminata()
+
 archivo.close()
+def caminata_completa():
+    x = [0]
+    y = [0.5]
+    while abs(x[-1]) < 5 and y[-1] < 1 or y[-1] > 0:
+        P,bandera =  probabilidades(x[-1],y[-1])
+        if bandera == True:
+            break
+        salto = np.random.choice([0,1,2,3],p = P )
+        if salto == 0:
+            y.append(y[-1] + u1)
+            x.append(x[-1] + u2) 
+        if salto == 1:
+            y.append(y[-1] + u1)
+            x.append(x[-1] - u2)
+        if salto == 2:
+            y.append(y[-1] - u1)
+            x.append(x[-1] + u2)
+        if salto == 3:
+            y.append(y[-1] - u1)
+            x.append(x[-1] - u2)
+    plt.xlim(-5,5)
+    plt.ylim(0,1)
+    plt.plot(x,y)
+    plt.show()
+
+
